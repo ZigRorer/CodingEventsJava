8c2c7781a85ea3caf32c82c94fed46ee82426136
ZigRorer <zdrorer@gmail.com>
2024-10-17 19:50:38 -0400

    Ch15-16 examples

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -1,5 +1,9 @@
 package org.launchcode.codingevents.models;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 import java.util.Objects;
 
 /**
@@ -10,12 +14,27 @@ public class Event {
     private int id;
     private static int nextId = 1;
 
+    @NotBlank(message = "Name is required.")
+    @Size(min = 3, max = 50, message = "Name must be between 3 & 50 characters.")
     private String name;
+    @Size(max = 500, message = "Description too long!")
     private String description;
+    @NotBlank(message = "Email is required.")
+    @Email(message = "Invalid email. Try again.")
+    private String contactEmail;
 
-    public Event(String name, String description) {
+    private EventType type;
+
+    public Event(String name, String description, String contactEmail) {
+        this();
         this.name = name;
         this.description = description;
+        this.contactEmail = contactEmail;
+        this.type = type;
+
+    }
+
+    public Event() {
         this.id = nextId;
         nextId++;
     }
@@ -34,6 +53,22 @@ public class Event {
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public @Email String getContactEmail() {
+        return contactEmail;
+    }
+
+    public void setContactEmail(@Email String contactEmail) {
+        this.contactEmail = contactEmail;
+    }
+
+    public EventType getType() {
+        return type;
+    }
+
+    public void setType(EventType type) {
+        this.type = type;
     }
 
     public int getId() {
