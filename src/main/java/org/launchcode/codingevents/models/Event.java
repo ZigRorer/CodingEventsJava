226e8d981b82f7a0f2e41886a1df7e9412097407
ZigRorer <zdrorer@gmail.com>
2024-10-28 18:20:16 -0400

    Ch17 examples

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -3,12 +3,14 @@ package org.launchcode.codingevents.models;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 import java.util.Objects;
 
 /**
  * Created by Chris Bay
  */
+
 public class Event {
 
     private int id;
